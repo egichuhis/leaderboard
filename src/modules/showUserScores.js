2d42c9b34baf4cdfbc171777a94bcaf34e2f9daf
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-08-31 09:05:46 +0300

    Scores updating on list correctly

--- src/modules/showUserScores.js
+++ src/modules/showUserScores.js
@@ -1,6 +1,6 @@
 const showUserScores = (scores) => {
   const labelsDiv = document.getElementById('labels-div');
-
+  labelsDiv.innerHTML = '';
   scores.forEach((scoreObj) => {
     const label = document.createElement('label');
     label.innerHTML = `${scoreObj.user}:${scoreObj.score}`;
