cac99f294d1d8b773eb5b339a222e344e5ba0560
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-08-31 12:25:24 +0300

    Add sweetalert to project

--- src/modules/createGameScores.js
+++ src/modules/createGameScores.js
@@ -1,8 +1,21 @@
+import Swal from 'sweetalert2';
 import clearInputs from './clearInputs.js';
 import getGameID from './getGameID.js';
 
 const id = getGameID();
 const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores/`;
+
+const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.addEventListener('mouseenter', Swal.stopTimer);
+    toast.addEventListener('mouseleave', Swal.resumeTimer);
+  },
+});
 
 const createGameScore = async () => {
   const user = document.getElementById('user-input').value;
@@ -21,7 +34,12 @@ const createGameScore = async () => {
   try {
     const response = await fetch(url, requestOptions);
 
-    if (!response.ok) {
+    if (response.ok) {
+      Toast.fire({
+        icon: 'success',
+        title: 'Scored posted successfully',
+      });
+    } else {
       throw new Error('Network response was not ok');
     }
 
